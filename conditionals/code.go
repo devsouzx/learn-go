9cd725457decfe316b2181e7030c78c87f10b016
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-03-08 20:48:13 -0300

    conditionals

---
+++ conditionals/code.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+
+	conditionals()
+}
+
+func conditionals() {
+	messageLen := 10
+	maxMessageLen := 20
+	fmt.Println("Trying to send a message of length:", messageLen, "and a max length of:", maxMessageLen)
+
+	if messageLen < maxMessageLen {
+		fmt.Println("Message sent")
+	} else {
+		fmt.Println("Message not sent")
+	}
+}
