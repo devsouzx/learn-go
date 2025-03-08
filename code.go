07e8a6fffc2d4f5c3e230afb42b13bb09c00e1c3
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-03-08 20:05:02 -0300

    Short Variable Declaration

--- code.go
+++ code.go
@@ -5,14 +5,22 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World!")
 
-	variables()
+	shortVariableDeclaration()
 }
 
-func variables() {
+func basicVariables() {
 	var smsSendingLimit int
 	var costPerSMS float64
 	var hasPermission bool
 	var username string
 
 	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
+}
+
+func shortVariableDeclaration() {
+	messageStart :=  "Happy birthday! You are now"
+	age := 21
+	messageEnd := "years old!"
+	
+	fmt.Println(messageStart, age, messageEnd)
 }
