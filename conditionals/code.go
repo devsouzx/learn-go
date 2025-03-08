b2b8efd5c21b03d4970a576544e4166640140401
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-03-08 20:57:43 -0300

    switch

--- conditionals/code.go
+++ conditionals/code.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World!")
 
-	conditionals()
+	switchs()
 }
 
 func conditionals() {
@@ -17,5 +17,31 @@ func conditionals() {
 		fmt.Println("Message sent")
 	} else {
 		fmt.Println("Message not sent")
+	}
+}
+
+func switchs() {
+	plan := "basic"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+	plan = "pro"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+	plan = "enterprise"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+	plan = "free"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+	plan = "unknown"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+}
+
+func billingCost(plan string) float64 {
+	switch plan {
+	case "basic":
+		return 10.0
+	case "pro":
+		return 20.0
+	case "enterprise":
+		return 50.0
+	default:
+		return 0.0
 	}
 }
