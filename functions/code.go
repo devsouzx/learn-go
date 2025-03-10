9b3bdd8d9d707914c984cd4e2c593db808239225
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-03-10 15:44:42 -0300

    unit test

--- functions/code.go
+++ functions/code.go
@@ -10,8 +10,25 @@ func main() {
 	test("Lane,", " happy birthday!")
 	test("Zuck,", " hope that Metaverse thing works out")
 	test("Go", " is fantastic")
+
+	fmt.Println(getMonthlyPrice("basic"))
+	fmt.Println(getMonthlyPrice("premium"))
+	fmt.Println(getMonthlyPrice("enterprise"))
 }
 
 func test(s1 string, s2 string) {
 	fmt.Println(concat(s1, s2))
+}
+
+func getMonthlyPrice(tier string) int {
+	switch tier {
+	case "basic":
+		return 10000
+	case "premium":
+		return 15000
+	case "enterprise":
+		return 50000
+	default:
+		return 0
+	}
 }
