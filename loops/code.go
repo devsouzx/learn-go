280e85fe9f402ebe14d07f1ecf9890fe9a694029
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-03-17 08:25:09 -0300

    Omitting Conditions from a for Loop

--- loops/code.go
+++ loops/code.go
@@ -8,6 +8,8 @@ func main() {
 	}
 
 	fmt.Println(bulkSend(21))
+
+	fmt.Println(maxMessages(10.0))
 }
 
 func bulkSend(numMessages int) float64 {
@@ -16,4 +18,14 @@ func bulkSend(numMessages int) float64 {
 		totalCost += 1.0 + (0.01 * float64(i))
 	}
 	return totalCost
+}
+
+func maxMessages(thresh float64) int {
+	totalCost := 0.0
+	for i := 0; ; i++ {
+		totalCost += 1.0 + (0.01 * float64(i))
+		if totalCost > thresh {
+			return i
+		}
+	}
 }
