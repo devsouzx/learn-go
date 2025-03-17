27e4e9afc2a8149d6e10960747bdb065cbd38a05
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-03-17 07:43:04 -0300

    errors

---
+++ errors/code.go
@@ -0,0 +1,28 @@
+package main
+
+import ("fmt")
+
+func main() {
+	fmt.Println(sendSMSToCouple("rere", "eeeee"))
+}
+
+func sendSMSToCouple(msgToCustomer, msgToSpouse string) (int, error) {
+	costForCustomer, err := sendSMS(msgToCustomer)
+	if err != nil {
+		return 0.0, err
+	}
+	costForSpouse , err := sendSMS(msgToCustomer)
+	if err != nil {
+		return 0.0, err
+	}
+	return costForCustomer + costForSpouse, nil
+}
+
+func sendSMS(message string) (int, error) {
+	const maxTextLen = 25
+	const costPerChar = 2
+	if len(message) > maxTextLen {
+		return 0, fmt.Errorf("can't send texts over %v characters", maxTextLen)
+	}
+	return costPerChar * len(message), nil
+}
